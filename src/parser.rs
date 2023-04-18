81c4aa1cf27006cf3042c2ea16b7ed7e0d35b0f9
Jonathan Johnson <jon@khonsulabs.com>
2023-04-18 09:32:12 -0700

    Fixing no-std compilation

--- src/parser.rs
+++ src/parser.rs
@@ -1,6 +1,7 @@
 use alloc::borrow::Cow;
 use alloc::vec::Vec;
 use core::fmt::{Display, Formatter};
+use core::mem;
 use core::ops::Range;
 
 use crate::tokenizer::{self, Balanced, Integer, Token, TokenKind, Tokenizer};
@@ -320,7 +321,7 @@ impl<'s> Iterator for Parser<'s> {
                     }
                 }
                 State::StartingImplicitMap(_) => {
-                    let State::StartingImplicitMap(first_key) = std::mem::replace(&mut self.root_state, State::ImplicitMap(MapState::ExpectingColon))
+                    let State::StartingImplicitMap(first_key) = mem::replace(&mut self.root_state, State::ImplicitMap(MapState::ExpectingColon))
                         else { unreachable!("just matched") };
                     Ok(Event::Primitive(Primitive::Identifier(first_key)))
                 }
