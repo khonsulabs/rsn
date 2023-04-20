984861c3a6e7251cc8e4b273e2140621c69c481d
kl-botsu <botsu@khonsulabs.com>
2023-04-20 16:46:01 +0000

    Deploying to gh-pages from @ khonsulabs/rsn@45eddee57cce4eb1684c08e4b8103cc859c91181 🚀

---
+++ main/implementors/core/ops/deref/trait.Deref.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"rsn":[["impl&lt;'s&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"rsn/parser/struct.Name.html\" title=\"struct rsn::parser::Name\">Name</a>&lt;'s&gt;"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
