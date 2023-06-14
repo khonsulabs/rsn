4b9830c6b889c42f50921c180002ccd9b6e34624
kl-botsu <botsu@khonsulabs.com>
2023-06-14 20:21:37 +0000

    Deploying to gh-pages from @ khonsulabs/rsn@71b5685d039400076404fe29cd56a36aabc44462 🚀

--- main/implementors/serde/de/trait.SeqAccess.js
+++ main/implementors/serde/de/trait.SeqAccess.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"rsn":[["impl&lt;'a, 'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.163/serde/de/trait.SeqAccess.html\" title=\"trait serde::de::SeqAccess\">SeqAccess</a>&lt;'de&gt; for <a class=\"struct\" href=\"rsn/de/struct.SequenceDeserializer.html\" title=\"struct rsn::de::SequenceDeserializer\">SequenceDeserializer</a>&lt;'a, 'de&gt;"]]
+"rsn":[["impl&lt;'a, 'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.164/serde/de/trait.SeqAccess.html\" title=\"trait serde::de::SeqAccess\">SeqAccess</a>&lt;'de&gt; for <a class=\"struct\" href=\"rsn/de/struct.SequenceDeserializer.html\" title=\"struct rsn::de::SequenceDeserializer\">SequenceDeserializer</a>&lt;'a, 'de&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- main/implementors/serde/ser/trait.Error.js
+++ main/implementors/serde/ser/trait.Error.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"rsn":[["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.163/serde/ser/trait.Error.html\" title=\"trait serde::ser::Error\">Error</a> for <a class=\"enum\" href=\"rsn/value/enum.ToValueError.html\" title=\"enum rsn::value::ToValueError\">ToValueError</a>"]]
+"rsn":[["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.164/serde/ser/trait.Error.html\" title=\"trait serde::ser::Error\">Error</a> for <a class=\"enum\" href=\"rsn/value/enum.ToValueError.html\" title=\"enum rsn::value::ToValueError\">ToValueError</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
