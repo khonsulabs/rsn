3ce0adc8b2cdb7387d76351f55f71e8572667d20
kl-botsu <botsu@khonsulabs.com>
2024-10-22 13:17:16 +0000

    Deploying to gh-pages from @ khonsulabs/rsn@04004a5ab1b5ebffb92ec5815200236905840914 🚀

--- main/trait.impl/serde/de/trait.EnumAccess.js
+++ main/trait.impl/serde/de/trait.EnumAccess.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["rsn",[["impl&lt;'a, 'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.210/serde/de/trait.EnumAccess.html\" title=\"trait serde::de::EnumAccess\">EnumAccess</a>&lt;'de&gt; for &amp;'a mut <a class=\"struct\" href=\"rsn/de/struct.Deserializer.html\" title=\"struct rsn::de::Deserializer\">Deserializer</a>&lt;'de&gt;"]]]]);
+    var implementors = Object.fromEntries([["rsn",[["impl&lt;'a, 'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.211/serde/de/trait.EnumAccess.html\" title=\"trait serde::de::EnumAccess\">EnumAccess</a>&lt;'de&gt; for &amp;'a mut <a class=\"struct\" href=\"rsn/de/struct.Deserializer.html\" title=\"struct rsn::de::Deserializer\">Deserializer</a>&lt;'de&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
--- main/trait.impl/serde/de/trait.SeqAccess.js
+++ main/trait.impl/serde/de/trait.SeqAccess.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["rsn",[["impl&lt;'a, 'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.210/serde/de/trait.SeqAccess.html\" title=\"trait serde::de::SeqAccess\">SeqAccess</a>&lt;'de&gt; for <a class=\"struct\" href=\"rsn/de/struct.SequenceDeserializer.html\" title=\"struct rsn::de::SequenceDeserializer\">SequenceDeserializer</a>&lt;'a, 'de&gt;"]]]]);
+    var implementors = Object.fromEntries([["rsn",[["impl&lt;'a, 'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.211/serde/de/trait.SeqAccess.html\" title=\"trait serde::de::SeqAccess\">SeqAccess</a>&lt;'de&gt; for <a class=\"struct\" href=\"rsn/de/struct.SequenceDeserializer.html\" title=\"struct rsn::de::SequenceDeserializer\">SequenceDeserializer</a>&lt;'a, 'de&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
