79339f52cac675284512440118be468a5da096a4
kl-botsu <botsu@khonsulabs.com>
2024-10-24 15:23:12 +0000

    Deploying to gh-pages from @ khonsulabs/rsn@c112c901d598b803115e22ba8e46ccb03637d606 🚀

--- main/trait.impl/serde/de/trait.SeqAccess.js
+++ main/trait.impl/serde/de/trait.SeqAccess.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["rsn",[["impl&lt;'a, 'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.212/serde/de/trait.SeqAccess.html\" title=\"trait serde::de::SeqAccess\">SeqAccess</a>&lt;'de&gt; for <a class=\"struct\" href=\"rsn/de/struct.SequenceDeserializer.html\" title=\"struct rsn::de::SequenceDeserializer\">SequenceDeserializer</a>&lt;'a, 'de&gt;"]]]]);
+    var implementors = Object.fromEntries([["rsn",[["impl&lt;'a, 'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.213/serde/de/trait.SeqAccess.html\" title=\"trait serde::de::SeqAccess\">SeqAccess</a>&lt;'de&gt; for <a class=\"struct\" href=\"rsn/de/struct.SequenceDeserializer.html\" title=\"struct rsn::de::SequenceDeserializer\">SequenceDeserializer</a>&lt;'a, 'de&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
