d197e8d191ac4ebbd0d268a07d9e8ed6915eeeed
kl-botsu <botsu@khonsulabs.com>
2024-10-30 16:42:17 +0000

    Deploying to gh-pages from @ khonsulabs/rsn@162e041a00137503fb12a77ff2174a17ae36cd95 🚀

--- main/trait.impl/serde/de/trait.MapAccess.js
+++ main/trait.impl/serde/de/trait.MapAccess.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["rsn",[["impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.213/serde/de/trait.MapAccess.html\" title=\"trait serde::de::MapAccess\">MapAccess</a>&lt;'de&gt; for <a class=\"struct\" href=\"rsn/de/struct.Deserializer.html\" title=\"struct rsn::de::Deserializer\">Deserializer</a>&lt;'de&gt;"]]]]);
+    var implementors = Object.fromEntries([["rsn",[["impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.214/serde/de/trait.MapAccess.html\" title=\"trait serde::de::MapAccess\">MapAccess</a>&lt;'de&gt; for <a class=\"struct\" href=\"rsn/de/struct.Deserializer.html\" title=\"struct rsn::de::Deserializer\">Deserializer</a>&lt;'de&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
--- main/trait.impl/serde/ser/trait.Error.js
+++ main/trait.impl/serde/ser/trait.Error.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["rsn",[["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.213/serde/ser/trait.Error.html\" title=\"trait serde::ser::Error\">Error</a> for <a class=\"enum\" href=\"rsn/value/enum.ToValueError.html\" title=\"enum rsn::value::ToValueError\">ToValueError</a>"]]]]);
+    var implementors = Object.fromEntries([["rsn",[["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.214/serde/ser/trait.Error.html\" title=\"trait serde::ser::Error\">Error</a> for <a class=\"enum\" href=\"rsn/value/enum.ToValueError.html\" title=\"enum rsn::value::ToValueError\">ToValueError</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
